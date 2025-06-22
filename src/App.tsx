905c5b754ab0d3cd48c2bff983d74cb335abbfcf
ALBIN JACOB <ajrockies@gmail.com>
2025-06-22 22:55:46 +0530

    Interactive Travel Itinerary Visualization

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
-import TravelItinerary from './components/TravelItinerary';
-import BookingWidget from './components/BookingWidget';
-import Sidebar from './components/Sidebar';
+import InteractiveTravelItinerary from './components/InteractiveTravelItinerary';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Header />
-      
-      <main className="max-w-7xl mx-auto px-6 py-6">
-        <div className="grid grid-cols-1 xl:grid-cols-4 gap-6">
-          {/* Main Content */}
-          <div className="xl:col-span-3 space-y-6">
-            {/* Travel Itinerary */}
-            <TravelItinerary />
-            
-            {/* Booking Widget */}
-            <BookingWidget />
-          </div>
-          
-          {/* Sidebar */}
-          <div className="xl:col-span-1">
-            <Sidebar />
-          </div>
-        </div>
-      </main>
+      <InteractiveTravelItinerary />
     </div>
   );
 }
